02a8fab67af4826536ad12770a383c328d7f4c47
Hongsambaek <166903161+Hongsambaek@users.noreply.github.com>
2024-06-08 14:42:05 +0900

    SingletonTest

---
+++ 09. SingletonTest/src/kr/or/ddit/member/MemberMain.java
@@ -0,0 +1,238 @@
+package kr.or.ddit.member;
+
+import java.util.List;
+import java.util.Scanner;
+
+import kr.or.ddit.member.service.IMemberService;
+import kr.or.ddit.member.service.MemberServiceImpl;
+import kr.or.ddit.member.vo.MemberVO;
+
+/*
+   회원정보를 관리하는 프로그램을 작성하는데 
+   아래의 메뉴를 모두 구현하시오. (CRUD기능 구현하기)
+   (DB의 MYMEMBER테이블을 이용하여 작업한다.)
+   
+   * 자료 삭제는 회원ID를 입력 받아서 삭제한다.
+   
+   예시메뉴)
+   ----------------------
+      == 작업 선택 ==
+      1. 자료 입력         ---> insert
+      2. 자료 삭제         ---> delete
+      3. 자료 수정         ---> update
+      4. 전체 자료 출력   ---> select
+      5. 작업 끝.
+   ----------------------
+    
+      
+// 회원관리 프로그램 테이블 생성 스크립트 
+create table mymember(
+    mem_id varchar2(8) not null,  -- 회원ID
+    mem_name varchar2(100) not null, -- 이름
+    mem_tel varchar2(50) not null, -- 전화번호
+    mem_addr varchar2(128),    -- 주소
+    reg_dt DATE DEFAULT sysdate, -- 등록일
+    CONSTRAINT MYMEMBER_PK PRIMARY KEY (mem_id)
+);
+
+*/
+public class MemberMain {
+
+
+	private Scanner scan;
+	private IMemberService memService;
+	
+	public MemberMain() {
+		memService = new MemberServiceImpl();
+		scan = new Scanner(System.in);
+	}
+
+	/**
+	 * 메뉴를 출력하는 메서드
+	 */
+	public void displayMenu() {
+		System.out.println();
+		System.out.println("----------------------");
+		System.out.println("  === 작 업 선 택 ===");
+		System.out.println("  1. 자료 입력");
+		System.out.println("  2. 자료 삭제");
+		System.out.println("  3. 자료 수정");
+		System.out.println("  4. 전체 자료 출력");
+		System.out.println("  5. 작업 끝.");
+		System.out.println("----------------------");
+		System.out.print("원하는 작업 선택 >> ");
+	}
+
+	/**
+	 * 프로그램 시작메서드
+	 */
+	public void start() {
+		int choice;
+		do {
+			displayMenu(); // 메뉴 출력
+			choice = scan.nextInt(); // 메뉴번호 입력받기
+			switch (choice) {
+			case 1: // 자료 입력
+				insertMember();
+				break;
+			case 2: // 자료 삭제
+				deleteMember();
+				break;
+			case 3: // 자료 수정
+				updateMember();
+				break;
+			case 4: // 전체 자료 출력
+				displayAllMember();
+				break;
+			case 5: // 작업 끝
+				System.out.println("작업 END!");
+				break;
+			default:
+				System.out.println("JAX");
+			}
+		} while (choice != 5);
+	}
+
+	/**
+	 * 모든 회원정보를 출력하는 method
+	 */
+	private void displayAllMember() {
+
+		System.out.println();
+		System.out.println("----------------------------------------");
+		System.out.println("ID\t생성일 \t\t이 름 \t전화번호 \t주  소");
+		System.out.println("----------------------------------------");
+		
+		List<MemberVO> memList = memService.getTotalMember();
+		
+		if(memList.size() == 0) {
+			System.out.println("회원정보가 존재하지 않습니다.");
+		}else {
+			for(MemberVO mv : memList) {
+				System.out.println(mv.getMemId() + "\t" + mv.getRegDt() + "\t" + mv.getMemName()+ "\t" + mv.getMemTel() + "\t" + mv.getMemAddr());
+				
+			}
+		}
+		System.out.println("────────────────────────────────────────────────────────────");
+		System.out.println("출력 끝 ! ! !");
+
+	}
+
+	private void deleteMember() {
+		
+
+		System.out.println();
+		System.out.println("Delete ??");
+		System.out.print("아이디 >>");
+		String memId = scan.next();
+
+		/////////////////////////////////////////////////
+		
+		int cnt = memService.removeMember(memId);
+		
+		if (cnt > 0) {
+			System.out.println(memId + "인 회원정보 삭제 success");
+		} else {
+			System.out.println(memId + "인 회원정보 삭제 fail");
+		}
+
+	}
+
+	private void updateMember() {
+		boolean isExist = false;
+		String memId = "";
+		do {
+			System.out.println();
+			System.out.println("수정할 회원 정보를 입력해주세요.");
+			System.out.print("회원ID >> ");
+			memId = scan.next();
+
+			isExist = memService.checkMember(memId);
+			if (!isExist) {
+				System.out.println("존재하지 않는 ID입니다.");
+				continue;
+			}
+		} while (!isExist);
+
+		System.out.print("회원 이름 >>");
+		String memName = scan.next();
+
+		System.out.print("회원 전화번호 >>");
+		String memTel = scan.next();
+
+		scan.nextLine(); // 엔터키 제거용
+
+		System.out.print("회원 주소 >>");
+		String memAddr = scan.nextLine();
+
+			
+		MemberVO mv = new MemberVO();
+		mv.setMemId(memId);
+		mv.setMemName(memName);
+		mv.setMemTel(memTel);
+		mv.setMemAddr(memAddr);
+		
+			int cnt = memService.modifyMember(mv);
+			
+
+			if (cnt > 0) {
+				System.out.println(memId + "인 회원정보 수정 success");
+			} else {
+				System.out.println(memId + "인 회원정보 수정 fail");
+			}
+		
+
+	}
+
+	private void insertMember() {
+		boolean isExist = false;
+		String memId = "";
+		do {
+			System.out.println();
+			System.out.println("추가할 회원 정보를 입력해주세요.");
+			System.out.print("회원ID >> ");
+			memId = scan.next();
+
+			isExist = memService.checkMember(memId);
+			if (isExist) {
+				System.out.println("이미 존재하는 ID입니다.");
+				continue;
+			}
+		} while (isExist);
+
+		System.out.print("회원 이름 >>");
+		String memName = scan.next();
+
+		System.out.print("회원 전화번호 >>");
+		String memTel = scan.next();
+
+		scan.nextLine(); // 엔터키 제거용
+
+		System.out.print("회원 주소 >>");
+		String memAddr = scan.nextLine();
+		
+		///////////////////////////////////////////////////////
+		
+		MemberVO mv = new MemberVO();
+		mv.setMemId(memId);
+		mv.setMemName(memName);
+		mv.setMemTel(memTel);
+		mv.setMemAddr(memAddr);
+		
+		int cnt = memService.registerMember(mv);
+		
+		if (cnt > 0) {
+			System.out.println(memId + "인 회원정보 등록 success");
+		} else {
+			System.out.println(memId + "인 회원정보 등록 fail");
+		}
+
+	}
+
+
+	public static void main(String[] args) {
+		MemberMain memObj = new MemberMain();
+		memObj.start();
+	}
+
+}
---
+++ 09. SingletonTest/src/kr/or/ddit/member/service/MemberServiceImpl.java
@@ -0,0 +1,61 @@
+package kr.or.ddit.member.service;
+
+import java.util.List;
+
+import kr.or.ddit.member.dao.IMemberDao;
+import kr.or.ddit.member.dao.MemberDaoImplWithJDBC;
+import kr.or.ddit.member.vo.MemberVO;
+
+
+public class MemberServiceImpl implements IMemberService{
+	
+	private static IMemberService memService = new MemberServiceImpl();
+	
+	private static IMemberDao memDao;
+	
+	public MemberServiceImpl() {
+		memDao = MemberDaoImplWithJDBC.getInstance();
+	}
+	
+	public static IMemberService GetInstance() {
+		return memService;
+	}
+
+	@Override
+	public int registerMember(MemberVO mv) {
+		
+		int cnt = memDao.insertMember(mv);
+		
+		// 회원에게 가입완료 메일 발송하기...
+		
+		
+		return cnt;
+	}
+
+	@Override
+	public int modifyMember(MemberVO mv) {
+
+		int cnt = memDao.updateMember(mv);
+		
+		return cnt;
+	}
+
+	@Override
+	public boolean checkMember(String memId) {
+		
+		return memDao.checkMember(memId);
+	}
+
+	@Override
+	public int removeMember(String memId) {
+		
+		return memDao.deleteMember(memId);
+	}
+
+	@Override
+	public List<MemberVO> getTotalMember() {
+		
+		return memDao.getAllMember();
+	}
+
+}
